76432119a57816a9b8e99bfed31603c5d34ceb84
AzrizHaziq <haziq@nazrol.tech>
2017-09-19 11:57:08 +0800

    Add heroes abiliti and ability to random hero name and abilities
    bump a new features

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 var heroes = require('../data/heroes.json').heroes
+var abilities = require('../data/abilities.json').abilities
 
-var randomHero = function(){
-    return heroes[Math.floor(Math.random() * heroes.length)]
+var dota = {
+    heroes : heroes,
+    abilities : abilities
+}
+
+var random = function(key){
+
+    if( !(key in dota) || key === undefined){
+        return console.warn("must be either " + Object.keys(dota).join(' or '))
+    }
+
+    return dota[key][ Math.floor(Math.random() * dota[key].length) ]
 }
 
 module.exports = {
-    all : heroes,
-    randomHero : randomHero
+    heroes : heroes,
+    abilities : abilities,
+    random : random,
 }
