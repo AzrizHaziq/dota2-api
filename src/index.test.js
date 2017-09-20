e5a76db2fd4f56261eb890c3846178a020938cf3
AzrizHaziq <haziq@nazrol.tech>
2017-09-20 10:19:49 +0800

    test(update test case): dry test case
    now provide a basic test case for dota2 json api

--- src/index.test.js
+++ src/index.test.js
@@ -8,207 +8,245 @@ var abilities = require('../data/abilities.json').abilities
 var dota2Api = require('./index')
 var expect = require('chai').expect;
 
+var dotas = [
+    {
+        title : 'Dota Mods',
+        items : mods,
+        key : 'mods',
+        data : {
+            "id" : 18,
+            "name" : "Ability Draft"
+        },
+        tests : [
+            {
+                title : 'should be and array of Mod object',
+                callback : function(mod){
+                    return mod.every(function(obj){
+                        return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                    })
+                }
+            },
+            {
+                title : 'should contain `Ability Draft`'
+            },
+            {
+                title : 'should contain random mods',
+            },
+            {
+                title : 'should contain random array of 5 mods',
+            },
+            {
+                title : 'should return searched `Ability Draft` array'
+            }
+        ]
+    },
+    {
+        title : 'Dota Items',
+        items : items,
+        key : 'items',
+        data : {
+            "id" : 91,
+            "name" : "recipe_urn_of_shadows"
+        },
+        tests : [
+            {
+                title : 'should be and array of Item object',
+                callback : function(item){
+                    return item.every(function(obj){
+                        return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                    })
+                }
+            },
+            {
+                title : 'should contain `recipe_urn_of_shadows`'
+            },
+            {
+                title : 'should contain random Item',
+            },
+            {
+                title : 'should contain random array of 5 Items',
+            },
+            {
+                title : 'should return searched `recipe_urn_of_shadows` array'
+            }
+        ]
+    },
+    {
+        title : 'Dota Heroes',
+        items : heroes,
+        key : 'heroes',
+        data : {
+            "name" : "leshrac",
+            "id" : 52,
+            "localized_name" : "Leshrac"
+        },
+        tests : [
+            {
+                title : 'should be and array of Mod object',
+                callback : function(hero){
+                    return hero.every(function(obj){
+                        return (
+                            obj.hasOwnProperty('name') &&
+                            obj.hasOwnProperty('id') &&
+                            obj.hasOwnProperty('localized_name')
+                        )
+                    })
+                }
+            },
+            {
+                title : 'should contain `leshrac`'
+            },
+            {
+                title : 'should contain random mods',
+            },
+            {
+                title : 'should contain random array of 5 mods',
+            },
+            {
+                title : 'should return searched `leshrac` array'
+            }
+        ]
+    },
+    {
+        title : 'Dota Lobbies',
+        items : lobbies,
+        key : 'lobbies',
+        data : {
+            "id" : 6,
+            "name" : "Solo Queue"
+        },
+        tests : [
+            {
+                title : 'should be and array of Lobby object',
+                callback : function(lobby){
+                    return lobby.every(function(obj){
+                        return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                    })
+                }
+            },
+            {
+                title : 'should contain `Solo Queue`'
+            },
+            {
+                title : 'should contain random Lobbies',
+            },
+            {
+                title : 'should contain random array of 5 Lobbies',
+            },
+            {
+                title : 'should return searched `Solo Queue` array'
+            }
+        ]
+    },
+    {
+        title : 'Dota Regions',
+        items : regions,
+        key : 'regions',
+        data : {
+            "id" : 224,
+            "name" : "China"
+        },
+        tests : [
+            {
+                title : 'should be and array of Region object',
+                callback : function(mod){
+                    return mod.every(function(obj){
+                        return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                    })
+                }
+            },
+            {
+                title : 'should contain `China`'
+            },
+            {
+                title : 'should contain random Region',
+            },
+            {
+                title : 'should contain random array of 5 Region',
+            },
+            {
+                title : 'should return searched `China` array'
+            }
+        ]
+    },
+    {
+        title : 'Dota Abilities',
+        items : abilities,
+        key : 'abilities',
+        data : {
+            "id" : 5185,
+            "name" : "faceless_void_chronosphere"
+        },
+        tests : [
+            {
+                title : 'should be and array of Abilities object',
+                callback : function(ability){
+                    return ability.every(function(obj){
+                        return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                    })
+                }
+            },
+            {
+                title : 'should contain `faceless_void_chronosphere`',
+            },
+            {
+                title : 'should contain random Ability',
+            },
+            {
+                title : 'should contain random array of 5 Abilities',
+            },
+            {
+                title : 'should return searched `faceless_void_chronosphere` array'
+            }
+        ]
+    }
+]
+
 describe('Dota2 JSON Api', function(){
 
-    describe('Dota Mods', function(){
+    describe('Basic functionality of Dota2 JSON API', function(){
 
-        it('should be and array of Mod object', function(){
-            expect(dota2Api.mods).to.satisfy(isArrayOfMods)
+        dotas.forEach(function(dota){
 
-            function isArrayOfMods(mod){
-                return mod.every(function(obj){
-                    return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+            describe(dota.title, function(){
+
+                var key = dota.key,
+                    data = dota.data;
+
+                //check obj structure
+                it(dota.tests[0].title, function(){
+                    expect(dota.items).to.satisfy(dota.tests[0].callback)
                 })
-            }
-        })
 
-        it('should contain `Ability Draft`', function(){
-            return expect(dota2Api.mods).to.deep.include({
-                "id" : 18,
-                "name" : "Ability Draft"
-            })
-        })
-
-        it('should contain random mods', function(){
-            var randomMods = dota2Api.random('mods')
-            return expect(dota2Api.mods).to.deep.include(randomMods)
-        })
-
-        it('should contain random array of 5 mods ', function(){
-            var random5Mods = dota2Api.random('mods', 5)
-            return expect(random5Mods)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
-        })
-
-    })
-
-    describe('Dota Item', function(){
-
-        it('should be and array of Item object', function(){
-            expect(dota2Api.items).to.satisfy(isArrayOfItems)
-
-            function isArrayOfItems(item){
-                return item.every(function(ability){
-                    return ability.hasOwnProperty('id') && ability.hasOwnProperty('name')
+                //should contain an obj of
+                it(dota.tests[1].title, function(){
+                    return expect(dota.items).to.deep.include(data)
                 })
-            }
-        })
 
-        it('should contain `Cheese`', function(){
-            return expect(dota2Api.items).to.deep.include({
-                "id" : 33,
-                "name" : "cheese"
-            })
-        })
-
-        it('should contain random items', function(){
-            var randomItem = dota2Api.random('items')
-            return expect(dota2Api.items).to.deep.include(randomItem)
-        })
-
-        it('should contain random array of 5 Items ', function(){
-            var random5Items = dota2Api.random('items', 5)
-            return expect(random5Items)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
-        })
-
-    })
-
-    describe('Dota Heroes', function(){
-
-        it('should be and array of heroes object', function(){
-            expect(dota2Api.heroes).to.satisfy(isArrayOfHeroes)
-
-            function isArrayOfHeroes(heroes){
-                return heroes.every(function(item){
-                    return (
-                        item.hasOwnProperty('name') &&
-                        item.hasOwnProperty('id') &&
-                        item.hasOwnProperty('localized_name')
-                    )
+                //random item
+                it(dota.tests[2].title, function(){
+                    var randomItem = dota2Api.random(key)
+                    return expect(dota.items).to.deep.include(randomItem)
                 })
-            }
-        })
 
-        it('should contain `Anti Mage`', function(){
-            return expect(dota2Api.heroes).to.deep.include({
-                "id" : 1,
-                "name" : "antimage",
-                "localized_name" : "Anti-Mage"
-            })
-        })
-
-        it('should contain random heroes', function(){
-            var randomHeroes = dota2Api.random('heroes')
-            return expect(dota2Api.heroes).to.deep.include(randomHeroes)
-        })
-
-        it('should contain random array of 5 Items ', function(){
-            var random5Items = dota2Api.random('items', 5)
-            return expect(random5Items)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
-        })
-
-    })
-
-    describe('Dota Lobbies', function(){
-
-        it('should be and array of Lobby object', function(){
-            expect(dota2Api.lobbies).to.satisfy(isArrayOfLobbies)
-
-            function isArrayOfLobbies(lobby){
-                return lobby.every(function(obj){
-                    return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                //random 5 item
+                it(dota.tests[3].title, function(){
+                    var randomItems = dota2Api.random(key, 5)
+                    return expect(randomItems)
+                        .to.be.an('array')
+                        .to.have.lengthOf(5)
                 })
-            }
-        })
 
-        it('should contain `Public matchmaking`', function(){
-            return expect(dota2Api.lobbies).to.deep.include({
-                "name" : "Public matchmaking",
-                "id" : 0
-            })
-        })
-
-        it('should contain random lobbies', function(){
-            var randomLobbies = dota2Api.random('lobbies')
-            return expect(dota2Api.lobbies).to.deep.include(randomLobbies)
-        })
-
-        it('should contain random array of 5 Lobbies ', function(){
-            var random5Lobbies = dota2Api.random('lobbies', 5)
-            return expect(random5Lobbies)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
-        })
-
-    })
-
-    describe('Dota Regions', function(){
-
-        it('should be and array of region object', function(){
-            expect(dota2Api.regions).to.satisfy(isArrayOfRegions)
-
-            function isArrayOfRegions(region){
-                return region.every(function(obj){
-                    return obj.hasOwnProperty('id') && obj.hasOwnProperty('name')
+                //search item
+                it(dota.tests[4].title, function(){
+                    var searchedItem = dota2Api.search(key, data.name)
+                    return expect(searchedItem)
+                        .to.be.an('array')
+                        .to.deep.include(data)
                 })
-            }
-        })
 
-        it('should contain `US West`', function(){
-            return expect(dota2Api.regions).to.deep.include({
-                "name" : "US West",
-                "id" : 111
             })
-        })
 
-        it('should contain random regions', function(){
-            var randomRegion = dota2Api.random('regions')
-            return expect(dota2Api.regions).to.deep.include(randomRegion)
-        })
-
-        it('should contain random array of 5 Items ', function(){
-            var random5Regions = dota2Api.random('regions', 5)
-            return expect(random5Regions)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
-        })
-
-    })
-
-    describe('Dota Abilities', function(){
-
-        it('should be and array of abilities object', function(){
-            expect(dota2Api.abilities).to.satisfy(isArrayOfAbilities)
-
-            function isArrayOfAbilities(abilities){
-                return abilities.every(function(ability){
-                    return ability.hasOwnProperty('id') && ability.hasOwnProperty('name')
-                })
-            }
-        })
-
-        it('should contain `Necromonicon Worrior last will`', function(){
-            return expect(dota2Api.abilities).to.deep.include({
-                "name" : "necronomicon_warrior_last_will",
-                "id" : 5200
-            })
-        })
-
-        it('should contain random heroes', function(){
-            var randomAbilities = dota2Api.random('abilities')
-            return expect(dota2Api.abilities).to.deep.include(randomAbilities)
-        })
-
-        it('should contain random array of 5 Abilities ', function(){
-            var random5Abilities = dota2Api.random('abilities', 5)
-            return expect(random5Abilities)
-                .to.be.an('array')
-                .to.have.lengthOf(5)
         })
 
     })
